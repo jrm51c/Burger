a3c364825ee320f8fe340eeebcfd2056552d8504
jrm51c <jmccarthyn2n@yahoo.com>
2018-06-24 22:43:59 -0400

    created schema and seed files, created db and populated burgers table

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### Schema
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(30) NOT NULL,
+	devoured boolean default 0 not null,
+	PRIMARY KEY (id)
+);
+select * from burgers
